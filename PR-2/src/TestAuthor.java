4d84deefda6889ebd602c39b57b7889a19c589d9
Mafit <144586044+Mafit1@users.noreply.github.com>
2023-10-09 13:28:08 +0300

    Delete PR-2 directory

--- PR-2/src/TestAuthor.java
+++
@@ -1,6 +0,0 @@
-public class TestAuthor {
-    public static void main(String[] args) {
-        Author A1 = new Author("John", "John1984@gmail.com", 'M');
-        System.out.println(A1);
-    }
-}
