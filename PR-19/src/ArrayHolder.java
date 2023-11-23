5e8aadcb83351e563935331ea0ca4c1c9b4f80f6
Mafit <144586044+Mafit1@users.noreply.github.com>
2023-11-23 12:42:46 +0300

    Add files via upload

---
+++ PR-19/src/ArrayHolder.java
@@ -0,0 +1,22 @@
+public class ArrayHolder<T> {
+    private T[] array;
+
+    public ArrayHolder(T[] array) {
+        this.array = array;
+    }
+
+    public T getElement(int index) {
+        return array[index];
+    }
+
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        ArrayHolder<Integer> arrayHolder = new ArrayHolder<>(intArray);
+        System.out.println(arrayHolder.getElement(2));
+
+        String[] stringArray = {"Hello", "World"};
+        ArrayHolder<String> stringArrayHolder = new ArrayHolder<>(stringArray);
+        System.out.println(stringArrayHolder.getElement(0));
+        System.out.println(stringArrayHolder.getElement(1));
+    }
+}
